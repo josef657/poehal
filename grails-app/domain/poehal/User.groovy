dfcc5afa22511505c15381c68a76e58fced48a17
ios <josef657@yahoo.de>
2013-03-05 22:14:19 +0100

    First commit

---
+++ grails-app/domain/poehal/User.groovy
@@ -0,0 +1,17 @@
+package poehali
+
+class User {
+
+    static constraints = {
+    }
+    String id
+    String Name
+
+    String Surname
+    int telefon
+    String email
+
+    String email2
+    String email25
+
+}
